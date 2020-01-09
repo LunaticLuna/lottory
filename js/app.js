7c6b71d6c8cbaf918515499db176156843ea2f0a
Jie Li <lijiemessage@gmail.com>
2020-01-09 13:16:47 +0800

    finish

--- js/app.js
+++ js/app.js
@@ -1,15 +1,65 @@
+/*** helper functions***/
+function formatDate (timestamp) {
+  const d = new Date(timestamp)
+  const time = d.toLocaleTimeString('en-US')
+  return d.toLocaleDateString('zh-CN') + " | " + time.substr(0, 5) + time.slice(-2)  
+}
+function download(filename, text) {
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
+function historyToText(history){
+  let result = ""
+  for(let key in history){
+    const entry = history[key]
+    result += entry.time + "\n"
+    
+    const names = entry.winners.join(" ")
+    result += names + "\n\n"
+  }
+  return result
+}
+/*** helper function ends***/
+
+
+
 function openForm() {
-  console.log("open")
-  const mask = document.getElementsByClassName("login-popup")[0]
+  const mask = document.getElementById("custom-popup")
   mask.classList.remove("hide")
   // mask.classList.add("visible")
   document.getElementById("popupForm").style.display="block";
 }
 
 function closeForm() {
-  const mask = document.getElementsByClassName("login-popup")[0]
+  const mask = document.getElementById("custom-popup")
   mask.classList.add("hide")
   document.getElementById("popupForm").style.display="none";
+}
+function handleCloseHistory(){
+  const mask = document.getElementById("winner-popup")
+  mask.classList.add("hide")
+}
+function handleDownloadHistory(){
+  // alert("功能未实现")
+  const history = State.history
+  if (Object.entries(history).length === 0 && history.constructor === Object){
+    alert("未开始抽奖")
+    return
+  }
+  // console.log(historyToText(history))
+  download("中奖名单.txt",historyToText(State.history))
+}
+function handleBackToMain(){
+  handleCloseHistory()
+  State.backToMainView()
 }
 function handleCustomWinner(){
   const num = document.getElementById("custom-num").value
@@ -29,45 +79,96 @@ const State = {
   num: 0,//中奖人数
   lottoView:false,
   historyWinners: [],
+  history:{},
   winners:[],
   gender:"neutral",
+  drawing:false,
+  counters : [],
+  timers : [],
+  currEntry:null,
 
   changeToLottoView : (n) => {
     if (n === "n"){
-      console.log("ayayya")
       return
     }
     const num = parseInt(n)
-    console.log("users",users.length)
+    if (n > users.length - State.historyWinners.length){
+      alert("未中奖人数没有这么多哦")
+      return
+    }
     State.num = num
     State.lottoView = true
     setTimeout(ViewController.showLotto, 200)
     
   },
+  backToMainView : () => {
+    State.lottoView = false;
+    State.num = 0
+    State.winners = []
+    if (State.drawing === true){
+      alert("抽奖进行中！请抽出中奖者后再返回^_^")
+      return
+    }
+    ViewController.showMain()
+  },
   drawWinner: () => {
-    State.winner = []
-    //TODO: make sure State.num < user.length
-    //only do this when users.length is a small number
-    while(State.winners.length < State.num){
-        const r = Math.floor(Math.random() * users.length);
-        if((State.historyWinners.indexOf(r) === -1) && (State.winners.indexOf(r) === -1)) State.winners.push(r);
-    }
-    console.log(State.historyWinners)
-    console.log(State.winners)
-    State.historyWinners = State.historyWinners.concat(State.winners)
-    //determine what gender specific congratulation we should use
-    let allMale = true
-    let allFemale = true
-    for (let i = 0; i < State.num; i++){
-      if (users[State.winners[i]].gender === "male"){
-        allFemale = false
-      } else {
-        allMale = false
+
+    if (State.drawing === false){
+      if (State.num > users.length - State.historyWinners.length){
+        alert("未中奖人数没有这么多哦")
+        return
       }
+      //开始抽奖
+      State.drawing = true
+      State.winners = []
+      //TODO: make sure State.num < user.length
+      //only do this when users.length is a small number
+      
+
+      while(State.winners.length < State.num){
+          const r = Math.floor(Math.random() * users.length);
+          if((State.historyWinners.indexOf(r) === -1) && (State.winners.indexOf(r) === -1)) State.winners.push(r);
+      }
+      //
+      State.historyWinners = State.historyWinners.concat(State.winners)
+
+      //determine what gender specific congratulation we should use
+      let allMale = true
+      let allFemale = true
+      for (let i = 0; i < State.num; i++){
+        if (users[State.winners[i]].gender === "male"){
+          allFemale = false
+        } else {
+          allMale = false
+        }
+      }
+      State.gender = allMale ? "male" : (allFemale ? "female" : "neutral")
+      State.counters = [...Array(users.length).keys()]
+      State.timers = new Array(State.num).fill(null)
+      ViewController.startSlotMachine(State.counters, State.timers)
+      ViewController.toggleDrawButton("start")
+    }else{
+      //停止抽奖
+      State.drawing = false
+      const timestamp = Date.now()
+      State.history[timestamp] = {
+        time : formatDate(timestamp),
+        winners: State.winners.map((idx) => users[idx].name),
+      }
+      console.log(State.history)
+      ViewController.stopSlotMachine(State.winners,State.timers)
+      ViewController.toggleDrawButton("stop")
+
+
     }
-    State.gender = allMale ? "male" : (allFemale ? "female" : "neutral")
-    ViewController.startSlotMachine()
+  },
+  checkHistory(){
+    if(!State.drawing){
+      ViewController.showHistory(State.history)
+    }
+    
   }
+    
 }
 
 
@@ -75,12 +176,13 @@ class ViewController {
   static showLotto() {
     console.log("change to lottory view")
     //hide num select view and show lotto view
-    const a = document.getElementsByClassName("select-num")[0]
+    const a = document.getElementById("select")
     a.setAttribute("class",'hide')
     const lotto = document.getElementById("lotto")
     lotto.setAttribute("class",'lotto')
 
-
+    const backBtn = document.getElementById("back")
+    backBtn.disabled = false
     //add html for lotto view
     const winners = document.getElementById("machines")
     console.log(State.num)
@@ -90,17 +192,25 @@ class ViewController {
       e.id = `slot${i}`
       e.className = "slot-machine"
       // e.id = toString(i)
+      console.log(e)
       winners.appendChild(e)
     }
   }
-  static startSlotMachine(){
+  static startSlotMachine(counters, timers){
     const len = users.length
-    let counters = [...Array(len).keys()]//offset random
-    let timers = new Array(State.num).fill(null)
     // make all slots spin together
-    // for (let i = 0; i < State.num; i++){
-    //   timers[i] = setInterval(()=>spin(i+1.5, i, slotInterval),slotInterval)
-    // }
+    for (let i = 0; i < State.num; i++){
+      timers[i] = setInterval(()=>spin(i,),slotInterval)
+    }
+    function spin ( id){
+      const e = document.getElementById(`slot${id}`)
+      const currIdx = counters[id] % len
+      e.innerHTML = users[currIdx].name
+      counters[id] ++
+    }
+
+    //this code starts the next spinner after the previous one finishes
+    // timers[0] = setInterval(()=>spin(1, 0, slotInterval),slotInterval)
     // function spin (minSec, id, itv){
     //   const milSec = minSec * 1000
     //   const e = document.getElementById(`slot${id}`)
@@ -110,30 +220,22 @@ class ViewController {
     //   if ((counters[id] * itv >= milSec) && (currIdx === winnerIdx)){
     //     e.style.backgroundColor = "white"
     //     clearInterval(timers[id])
+    //     if (id < State.num - 1){
+    //       timers[id + 1] = setInterval(() => spin(1,id+1,slotInterval),slotInterval)
+    //     } else {
+    //       ViewController.showCongrats()
+    //     }
     //     return
     //   }
     //   counters[id] ++
     // }
+  }
+  static stopSlotMachine(winners, timers){
+    for (let i = 0; i < timers.length; i++){
+      clearInterval(timers[i])
+      const e = document.getElementById(`slot${i}`)
+      e.innerHTML = users[winners[i]].name
 
-    //this code starts the next spinner after the previous one finishes
-    timers[0] = setInterval(()=>spin(1, 0, slotInterval),slotInterval)
-    function spin (minSec, id, itv){
-      const milSec = minSec * 1000
-      const e = document.getElementById(`slot${id}`)
-      const winnerIdx = State.winners[id] //the idx of winner in users list
-      const currIdx = counters[id] % len
-      e.innerHTML = users[currIdx].name
-      if ((counters[id] * itv >= milSec) && (currIdx === winnerIdx)){
-        e.style.backgroundColor = "white"
-        clearInterval(timers[id])
-        if (id < State.num - 1){
-          timers[id + 1] = setInterval(() => spin(1,id+1,slotInterval),slotInterval)
-        } else {
-          ViewController.showCongrats()
-        }
-        return
-      }
-      counters[id] ++
     }
   }
   static showCongrats(){
@@ -142,7 +244,59 @@ class ViewController {
     console.log(State.gender)
     e.innerHTML = genderedCon[Math.floor(Math.random()*genderedCon.length)]
   }
+  static toggleDrawButton(state){
+    if (state === "start"){
+      const e = document.getElementById("lotto-btn")
+      e.innerHTML = "停！"
+    }else if (state === "stop"){
+      const e = document.getElementById("lotto-btn")
+      e.innerHTML = "继续抽奖"
+    }
+    
+  }
+  static showMain(){
+    console.log("change to main view")
+    const list = document.getElementById("machines")
+    list.innerHTML = ""
+    const b = document.getElementById("lotto")
+
+    b.setAttribute("class","hide")
+    const a = document.getElementById("select")
+    a.setAttribute('class',"select-num")
+    const backBtn = document.getElementById("back")
+    backBtn.disabled = true
+  }
   static showCustomize(){
+
+  }
+  static showHistory(history){
+    const popup = document.getElementById("winner-popup")
+    popup.classList.remove("hide")
+    const text = document.getElementById("text")
+    text.className = "text-container"
+    text.innerHTML = ""
+    //if empty history:
+    if (Object.entries(history).length === 0 && history.constructor === Object){
+      text.innerHTML = "<p>未开始抽奖</p>"
+    }
+    for (let key in history){
+      console.log("history.key:",key)
+      if (history.hasOwnProperty(key)){
+        
+        const entry = history[key]
+        console.log("entry:",entry)
+        const p1 = document.createElement("p")
+        p1.innerHTML = `${entry.time}:`
+        p1.classList.add("bold")
+        const p2 = document.createElement("p")
+        p2.innerHTML = `${entry.winners}`
+        text.appendChild(p1)
+        text.appendChild(p2)
+        const hr = document.createElement("hr")
+        text.appendChild(hr)
+        console.log(text)
+      }
+    }
 
   }
 }
@@ -164,8 +318,21 @@ class EventListener {
     const lottoBtn = document.getElementById("lotto-btn")
     console.log("bind clicking event to lotto btn")
     lottoBtn.addEventListener("click", (e) => {
-      EventHandler.drawWinner()
+      EventHandler.toggleDrawWinner()
     })
+    //bind back button with clicking event
+    const backBtn = document.getElementById("back")
+    console.log("bind back button")
+    backBtn.addEventListener("click", (e) =>{
+      EventHandler.backToNum()
+    })
+    //bind history button with clicking event
+    const historyBtn = document.getElementById("history")
+    console.log("bind history button")
+    historyBtn.addEventListener("click", (e) =>{
+      EventHandler.handleHistory()
+    })
+
   }
 }
 class EventHandler {
@@ -180,10 +347,18 @@ class EventHandler {
     }
     
   }
-  static drawWinner(){
+  static toggleDrawWinner(){
     console.log("handle drawWinner event")
+
     State.drawWinner()
 
+  }
+  static backToNum(){
+    console.log("handle back event")
+    State.backToMainView()
+  }
+  static handleHistory(){
+    State.checkHistory()
   }
 }
 function main(){
